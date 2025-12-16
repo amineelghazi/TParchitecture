9c3abb48387439a800a08afae2ba8a47ea939214
amineelghazi <amineelghazi100@hotmail.com>
2025-12-16 10:14:03 -0500

    Remove unnecessary whitespace in OffresVoyageurController

--- src/main/java/com/example/demoef/controller/OffresVoyageurController.java
+++ src/main/java/com/example/demoef/controller/OffresVoyageurController.java
@@ -15,27 +15,24 @@ public class OffresVoyageurController {
         this.offresService = offresService;
     }
 
-    // Liste toutes les offres
     @GetMapping("/all")
     public List<Offre> lire(){
         return offresService.getAllOffres();
     }
 
-    // Prix minimum
     @GetMapping("/prix_min")
     public double prixMin(){
         return offresService.getPrixMin();
     }
 
-    // Prix maximum
     @GetMapping("/prix_max")
     public double prixMax(){
         return offresService.getPrixMax();
     }
 
-    // Comparaison des prix (entre min et max)
     @GetMapping("/comparer")
     public List<Offre> comparer(@RequestParam double min, @RequestParam double max){
         return offresService.comparerPrix(min, max);
     }
+
 }
